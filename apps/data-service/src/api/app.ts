0b765a9f2cbc2f590da4bdb8d9fa7e57b5e1930f
MSkrzypietz <michael.skr97@gmail.com>
2025-08-22 13:23:13 +0200

    implement smart routing

---
+++ apps/data-service/src/api/app.ts
@@ -0,0 +1,23 @@
+import { Hono } from 'hono';
+import { getLink } from '@repo/data-ops/queries/links';
+import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
+import { getDestinationForCountry } from '@/helpers/route-ops';
+
+export const App = new Hono<{ Bindings: Env }>();
+
+App.get('/:id', async (c) => {
+	const id = c.req.param('id');
+	const link = await getLink(id);
+	if (!link) {
+		return c.text('Destination not found', 404);
+	}
+
+	const cfHeader = cloudflareInfoSchema.safeParse(c.req.raw.cf);
+	if (!cfHeader.success) {
+		return c.text('Invalid Cf headers', 400);
+	}
+
+	const headers = cfHeader.data;
+	const destination = getDestinationForCountry(link, headers.country);
+	return c.redirect(destination);
+});
