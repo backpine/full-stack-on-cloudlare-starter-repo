607b9778f9105087715940cb8b7f2894718bc0ef
MSkrzypietz <michael.skr97@gmail.com>
2025-08-24 19:29:52 +0200

    add click tracking

--- apps/data-service/src/api/app.ts
+++ apps/data-service/src/api/app.ts
@@ -1,10 +1,22 @@
 import { Hono } from 'hono';
 import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
 import { LinkClickMessageType } from '@repo/data-ops/zod-schema/queue';
-import { getDestinationForCountry, getRoutingDestinations } from '@/helpers/route-ops';
-import { EvaluationScheduler } from '@/durable-objects/evaluation-scheduler';
+import { captureLinkClickInBackground, getDestinationForCountry, getRoutingDestinations } from '@/helpers/route-ops';
 
 export const App = new Hono<{ Bindings: Env }>();
+
+App.get('/click-socket', async (c) => {
+	const upgradeHeader = c.req.header('Upgrade');
+	if (!upgradeHeader || upgradeHeader !== 'websocket') {
+		return c.text('Expected Upgrade: websocket', 426);
+	}
+
+	const accountId = c.req.header('account-id');
+	if (!accountId) return c.text('No Headers', 404);
+	const doId = c.env.LINK_CLICK_TRACKER_OBJECT.idFromName(accountId);
+	const stub = c.env.LINK_CLICK_TRACKER_OBJECT.get(doId);
+	return await stub.fetch(c.req.raw);
+});
 
 App.get('/:id', async (c) => {
 	const id = c.req.param('id');
@@ -33,6 +45,6 @@ App.get('/:id', async (c) => {
 			timestamp: new Date().toISOString(),
 		},
 	};
-	c.executionCtx.waitUntil(c.env.QUEUE.send(queueMessage));
+	c.executionCtx.waitUntil(captureLinkClickInBackground(c.env, queueMessage));
 	return c.redirect(destination);
 });
